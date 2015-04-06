ed8d906d9fa0328c3571a3e87253d0e788e0ef1e
Ivan Martinez <imartine@thoughtworks.com>
2015-04-05 23:22:44 -0500

    IM - #1: Initial project version

---
+++ SupermarketPricing/Pricing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketPricing
+{
+    public class Pricing
+    {
+        public string good { get; set; }
+        public int quantity { get; set; }
+
+        public Pricing(string good, int quantity)
+        {
+            this.good = good;
+            this.quantity = quantity;
+        }
+
+        public int GetPrice()
+        {
+            var price = 0;
+            if (good == "Beans" && quantity == 1)
+            {
+                price = 1;
+            }
+            return price;
+        }
+    }
+}
---
+++ SupermarketPricingTest/TestSupermarketPricing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SupermarketPricing;
+
+namespace SupermarketPricingTest
+{
+    [TestFixture]
+    public class TestSupermarketPricing
+    {
+        [Test]
+        public void TestSimplePricing()
+        {
+            var pricing = new Pricing("Beans", 1);
+            var price = pricing.GetPrice();
+            Assert.IsTrue(price == 1);
+        }
+
+    }
+}
